efbbf62bcc4f19875db5edc2ae908eafa9912a82
Noelia Parada <noeliacastrelo@gmail.com>
2023-01-07 21:40:08 +0100

    wrong indentation

--- app.js
+++ app.js
@@ -21,10 +21,9 @@ app.use("/api", allRoutes);
 
 app.use(
     cors({
-      origin: ["http://localhost:3000", process.env.ORIGIN],
+        origin: ["http://localhost:3000", process.env.ORIGIN],
     })
-  );
-  
+    );
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
