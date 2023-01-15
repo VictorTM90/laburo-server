9ee302caea6ab9878d50d1ca48e812c261e81b8b
Noelia Parada <noeliacastrelo@gmail.com>
2023-01-15 17:30:23 +0100

    expose port docker

--- server.js
+++ server.js
@@ -4,5 +4,6 @@ const app = require("./app");
 const PORT = process.env.PORT || 5005;
 
 app.listen(PORT, () => {
+  "0.0.0.0"
   console.log(`Server listening on port http://localhost:${PORT}`);
 });
